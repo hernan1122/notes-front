cf215eeab2ef123bcffad89b5a82430c1bd09232
axel torrico <torricohernan2@gmail.com>
2023-07-16 14:40:49 -0300

    se creo un boton provisorio para volver del home al inicio de la app

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css'
 
 function Home() {
   return (
     <div className='Home'>
       <h1>Hello from Home</h1>
+      <Link to={'/'}>Atras</Link>
     </div>
   );
 }
